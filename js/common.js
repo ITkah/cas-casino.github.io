a730c8aed681156bf3dcfd476a9b36f4a66db39a
KilkaSenpai <grekagrove@gmail.com>
2022-02-11 14:30:11 +0200

    upload

--- js/common.js
+++ js/common.js
@@ -1,16 +1,16 @@
-$('nav ul li').each(function() {
+$('nav ul li').each(function () {
     this_li = $(this);
     if ($(this_li).find('ul').length > 0) {
         $(this_li).append('<span class="arrow_mob"></span>');
     }
 });
 
-$('.arrow_mob').on('click', function(){
+$('.arrow_mob').on('click', function () {
     $(this).siblings('.sub-menu').slideToggle('150');
 });
 
-$('.search_input').on('keyup', function(){
-    var letter = $(this).val().length;   
+$('.search_input').on('keyup', function () {
+    var letter = $(this).val().length;
     if (letter >= 1) {
         $('.header_search').addClass('active_search');
     } else {
@@ -18,7 +18,11 @@ $('.search_input').on('keyup', function(){
     }
 });
 
-$('.call_menu, .close_menu').on('click', function(){
+$('.call_menu, .close_menu').on('click', function () {
     $('nav').toggleClass('nav_active');
     $('.bg').toggleClass('bg_active');
+});
+
+$('.methods_payment_btn').on('click', function () {
+    $('.methods_payment_drop').slideToggle();
 });
